5fbd1ab6b24ca8988abe0e1d0fc1a42ee7eac29b
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2025-12-28 10:27:57 +0530

    Razorpay BE done

--- backend/src/routes/payments.js
+++ backend/src/routes/payments.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { mockPaymentSuccess } = require("../controllers/payment.controller");
+const { mockPaymentSuccess,createRazorpayOrder,verifyRazorpayPayment } = require("../controllers/payment.controller");
 const router = express.Router();
-mockPaymentSuccess
+console.log(createRazorpayOrder, verifyRazorpayPayment);
 
-router.post("/mock-success", mockPaymentSuccess);
+// router.post("/mock-success", mockPaymentSuccess);
+router.post("/razorpay/order", createRazorpayOrder);
+router.post("/razorpay/verify", verifyRazorpayPayment);
 
 module.exports = router;
