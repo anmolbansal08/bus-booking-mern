d0af69d553eb5633373764270a6e57fde5fa595a
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2025-12-28 10:56:04 +0530

    Razorpay BE done

--- frontend-vite/src/pages/Payment.jsx
+++ frontend-vite/src/pages/Payment.jsx
@@ -15,21 +15,54 @@ export default function Payment() {
 
   const { booking, bus, travelDate, seats } = state;
 
-  const payNow = async () => {
-    try {
-      await api.post("/payments/mock-success", {
-        bookingId: booking._id
-      });
+//   const payNow = async () => {
+//     try {
+//       await api.post("/payments/mock-success", {
+//         bookingId: booking._id
+//       });
 
-navigate("/booking-success", {
-  state: { booking }
-});
-    } catch (err) {
-      alert(
-        err.response?.data?.message || "Payment failed"
-      );
-    }
-  };
+// navigate("/booking-success", {
+//   state: { booking }
+// });
+//     } catch (err) {
+//       alert(
+//         err.response?.data?.message || "Payment failed"
+//       );
+//     }
+//   };
+const payNow = async () => {
+  try {
+    const orderRes = await api.post(
+      "/payments/razorpay/order",
+      { bookingId: booking._id }
+    );
+
+    const options = {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: orderRes.data.amount,
+      currency: "INR",
+      name: "Bus Booking",
+      order_id: orderRes.data.id,
+      handler: async function (response) {
+        await api.post("/payments/razorpay/verify", {
+          razorpay_order_id: response.razorpay_order_id,
+          razorpay_payment_id: response.razorpay_payment_id,
+          razorpay_signature: response.razorpay_signature,
+          bookingId: booking._id
+        });
+
+        navigate("/booking-success", {
+          state: { booking }
+        });
+      }
+    };
+
+    const rzp = new window.Razorpay(options);
+    rzp.open();
+  } catch (err) {
+    alert("Payment failed");
+  }
+};
 
   return (
     <div className="max-w-xl mx-auto mt-16 px-4">
