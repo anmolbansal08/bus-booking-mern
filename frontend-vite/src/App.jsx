4b17e4ced450e3eba61e6cfcbead47525044771c
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2026-01-25 13:37:01 +0530

    add banner,remove extra white space

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -9,13 +9,14 @@ import BusListPage from "./pages/BusListPage";
 import PassengerInfo from "./pages/PassengerInfo";
 import BookingSuccess from "./pages/BookingSuccess";
 import Payment from "./pages/Payment";
+import Home from "./components/Home";
 export default function App() {
   return (
         <div className="min-h-screen bg-gray-50">
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Search />} />
+        <Route path="/" element={<Home />} />
         <Route path="/buses" element={<BusListPage />} />
         <Route path="/seats/:busId" element={<SeatSelect />} />
         <Route path="/login" element={<Login />} />
