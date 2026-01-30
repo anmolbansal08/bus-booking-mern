595e9211cbad6097547b7836dca28dc74be01317
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2026-01-30 10:19:01 +0530

    admin routes completed

--- backend/src/routes/admin.routes.js
+++ backend/src/routes/admin.routes.js
@@ -1,13 +1,16 @@
     const express = require("express");
 const router = express.Router();
 const { createRoute, listRoutes, getAllBookings} = require("../controllers/admin.controller");
-
 const adminOnly = require("../middleware/adminOnly");
 const auth = require("../middleware/auth");
+
+const { getAllBusesAdmin, createBus } = require("../controllers/bus.controller");
 // (optional) simple admin guard middleware later
 router.post("/routes", auth, adminOnly, createRoute);
 router.get("/routes", auth, adminOnly,listRoutes);
-// router.post("/buses", auth, adminOnly, createBus);
+
+router.get("/buses",auth,adminOnly,getAllBusesAdmin)
+router.post("/buses", auth, adminOnly, createBus);
 router.get("/bookings", auth, adminOnly, getAllBookings);
 
 module.exports = router;
