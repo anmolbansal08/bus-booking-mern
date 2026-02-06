4987a8e6d9a6c9bb36952a781fc33b081b8a055f
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2026-02-06 12:17:00 +0530

    add ticket generation logic

--- backend/src/routes/booking.routes.js
+++ backend/src/routes/booking.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { createBooking,getMyBookings,cancelBooking } = require("../controllers/booking.controller");
+const { createBooking,getMyBookings,cancelBooking, lookupBooking } = require("../controllers/booking.controller");
 const auth = require("../middleware/auth");
 
 
 router.post("/", auth, createBooking);
 router.get("/my", auth, getMyBookings);
 router.patch("/:bookingId/cancel", cancelBooking);module.exports = router;
+router.post("/lookup",lookupBooking);
