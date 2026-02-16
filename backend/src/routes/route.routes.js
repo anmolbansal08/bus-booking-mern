b785b83542ab32c039b847161e779582db624320
anmolbansal08 <37624937+anmolbansal08@users.noreply.github.com>
2026-02-16 17:49:37 +0530

    cities dropdown and destination based on source

--- backend/src/routes/route.routes.js
+++ backend/src/routes/route.routes.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const {
   createRoute,
-  searchRoutes
+  searchRoutes,
+  getDestinationsBySource
 } = require("../controllers/route.controller");
 
 router.post("/", createRoute);
 router.get("/search", searchRoutes);
 
+// routes/route.routes.js
+router.get("/destinations", getDestinationsBySource);
 module.exports = router;
